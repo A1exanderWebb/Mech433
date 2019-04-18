7ee49ea196df4a9d35bbc0b2c4b3c51c7fb71fe5
Alex Webb <Alex@Alexs-MBP.hsd1.il.comcast.net>
2019-04-18 09:50:00 -0500

    Homework 4

--- Class4.X/main.c
+++ Class4.X/main.c
@@ -1,4 +1,5 @@
 #include<xc.h>           // processor SFR definitions
+#include <math.h>
 #include<sys/attribs.h>  // __ISR macro
 
 // DEVCFG0
@@ -36,34 +37,87 @@
 #pragma config FUSBIDIO = ON // USB pins controlled by USB module
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
+#define M_PI  3.14159265358979323846
+#define CS LATBbits.LATB7       // chip select pin
+
+char SPI1_IO(char write){
+    SPI1BUF=write;
+    while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+    }
+    return SPI1BUF;
+}
+
+void setVoltage(char channel, int voltage){
+    unsigned char c1, c2;
+    unsigned char BUFGASHD=0b111;
+    c1=BUFGASHD<<4;
+    c1=c1|channel<<7;
+    c2=voltage & 0b11111111;
+    voltage=voltage>>8;
+    voltage=voltage & 0b1111;
+    c1=c1|voltage;
+    
+    LATBbits.LATB7=0;
+    SPI1_IO(c1);
+    SPI1_IO(c2);
+    LATBbits.LATB7=1;
+}
+
+void initSPI1(){
+  SPI1CON = 0;              // turn off the spi module and reset it
+  SPI1BUF;                  // clear the rx buffer by reading from it
+  SPI1BRG = 0x1;            // baud=
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on spi 1
+  TRISBbits.TRISB7 = 0;
+  TRISAbits.TRISA4 = 0;
+  RPB8Rbits.RPB8R=0b0011;
+}
+
 
 int main() {
-
+    unsigned int i = 0;
+    unsigned int trivltg;
+    unsigned int sinvltg;
+    char rising = 1;
+    
     __builtin_disable_interrupts();
-
     // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
-
     // 0 data RAM access wait states
     BMXCONbits.BMXWSDRM = 0x0;
     // enable multi vector interrupts
     INTCONbits.MVEC = 0x1;
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
-
     // do your TRIS and LAT commands here
-    TRISAbits.TRISA4 = 0;
-    LATAbits.LATA4 = 1;
-    TRISBbits.TRISB4 = 1;
     ANSELA=0;
+    ANSELB=0;
+    initSPI1();
     __builtin_enable_interrupts();
     
     while(1) {
+        if (i==1000){
+            i=0;
+            rising=-rising;
+        }
+        sinvltg=2048+1300*sin(M_PI*2*i/999);
+        
+        if(rising==1){
+            trivltg=i*4;
+        }
+        if(rising==-1){
+            trivltg=4000-i*4;
+        }
+        
+        setVoltage(0,sinvltg);
+        setVoltage(1,trivltg);
+        i++;
         _CP0_SET_COUNT(0);
-        LATAbits.LATA4 = 1;
-        while (_CP0_GET_COUNT()<=120000){;}
-        LATAbits.LATA4 = 0;
-        while (_CP0_GET_COUNT()<=120000){;}
-        while (PORTBbits.RB4 == 0){;}   
+        while(_CP0_GET_COUNT()<24000){;}
+        
     }
 }
