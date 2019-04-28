0fe0e9fe5a2f5c9a315bdbdc2857f555c8b96137
Alex Webb <Alex@dhcp-10-105-180-90.wireless.northwestern.private>
2019-04-28 15:06:36 -0500

    added photo
    to class 4

---
+++ Harmonytake1/firmware/Harmonytake1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Harmonytake1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Harmonytake1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=harmonytake1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/harmonytake1.x.tar
